10b01073d501dd5f0e5df43204478534e71cdfce
bingoshao <759875821@qq.com>
2019-07-31 14:14:21 +0800

    发现轮播完成

--- src/router.js
+++ src/router.js
@@ -7,6 +7,7 @@ import Video from "./pages/Video.vue";
 
 const router = new VueRouter({
     routes:[
+        {path:"/",redirect:'/discover'},
         { path:"/my",component:My},
         { path:"/discover",component:Discover},
         { path:"/cloudhome",component:CloudHome},
