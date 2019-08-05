f6478be6913214601607568810d2ea91d807ac77
logohtml <1044166132@qq.com>
2019-08-05 19:06:53 +0800

    注释

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,6 @@ module.exports = {
           target: 'http://120.27.243.6:3000',
           changeOrigin: true  //代理服务器会在请求头中加入相应Host首部，然后目标服务器就可以根据这个首部来区别要访问的站点了
         },
-      }   
+      } 
     }
 }
