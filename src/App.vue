ebe0da6cc3e0eb175f28c60191cf6b82c2458796
bingoshao <759875821@qq.com>
2019-08-05 18:50:05 +0800

    调整

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,7 @@ export default {
     Index
   },
   created() {
-     //在页面加载时读取localStorage里的状态信息
+     //在页面加载时读取localStorage里的状态信息。
     // localStorage.getItem("userMsg") && this.$store.replaceState(Object.assign(this.$store.state,JSON.parse(localStorage.getItem("userMsg"))));
     
     // //在页面刷新时将vuex里的信息保存到localStorage里
